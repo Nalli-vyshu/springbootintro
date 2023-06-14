1adf3468fc0726e675bb447a086f9ab3330e58a1
Nalli-vyshu <nallivyshnavi7@gmail.com>
2023-06-14 21:29:30 +0530

    complete

--- src/main/java/com/onlineshoppy/controller/shoppingController.java
+++ src/main/java/com/onlineshoppy/controller/shoppingController.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import com.onlineshoppy.model.laptop;
 import com.onlineshoppy.service.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class shoppingController {
-	
-	shoppingService service=new shoppingService();
+	@Autowired
+	shoppingService service;
 	
 	@RequestMapping(method=RequestMethod.GET,value="/laptops")
 	public List<laptop> getAlllaptops() {
@@ -22,21 +26,20 @@ public class shoppingController {
 	
 	
 	@RequestMapping(method=RequestMethod.GET,value="/laptops/(id)")
-	public String getLaptop() {
-	  return "will display specified laptop";
-}
+	public laptop getLaptop(@PathVariable int id) {
+		return service.getlaptop(id);
+	}
 	@RequestMapping(method=RequestMethod.POST,value="/laptops")
-	public String addLaptop() {
-		return "one laptop added";
+	public String addLaptop(@RequestBody laptop l1) {
+		return service.addLaptop(l1);
 	}
 	@RequestMapping(method=RequestMethod.PUT,value="/laptops/(id)")
-	public String updateLaptop() {
-		return "one laptop updated";
+	public void updateLaptop(@RequestBody laptop l2,@PathVariable int id) {
 		
 	}
 	@RequestMapping(method=RequestMethod.DELETE,value="/laptops/(id)")
-	public String deleteLaptop() {
-		return "one laptop deleted";
+	public void deleteLaptop(@PathVariable int id) {
+		service.deleteLaptop(id);
 	}
 }
 
--- src/main/java/com/onlineshoppy/service/shoppingService.java
+++ src/main/java/com/onlineshoppy/service/shoppingService.java
@@ -1,22 +1,50 @@
 package com.onlineshoppy.service;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.List;
 import com.onlineshoppy.model.*;
-import org.springframework.stereotype.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+ 
 @Service
 public class shoppingService {
-	public List<laptop> laptoplist=Arrays.asList(
+	public List<laptop> laptoplist= new ArrayList<>(Arrays.asList(
 			
 			                        new laptop(101,"Asus Tuf Gaming","i5",5600),
-			                        new laptop(102,"Lenova ideapad 330","i5",420)
-			                                                                      );
+			                        new laptop(102,"Lenova ideapad 330","i5",420),
+			                        new laptop(103,"Acer Predator","i7",72000),
+			                        new laptop(104,"MSI gaming duo","i5",5200)
+			                                                                    ));
+	private laptop lap;
 
 			public List<laptop> getAllLaptops() {
 				return laptoplist;
 			}
+		public laptop getlaptop(int id) {
+			return laptoplist.stream().filter(lap-> lap.getLaptopId()==id).findFirst().get();
+		}
+		public String addLaptop(laptop l1) {
+			laptoplist.add(l1);
+			return "sucessfully added";
+			
+		}
+		public void updateLaptop(int id,laptop l2) {
+			int i=0;
+			for(i=0;i<laptoplist.size ();i++) {
+				 laptop lap=laptoplist.get(i);
+				 
+				 if(lap.getLaptopId()==id) {
+					 laptoplist.set(i, l2);
+					 
 		
+				 }
+			}
+		}
+		
+		public void deleteLaptop(int id) {
+			laptoplist.removeIf(lap->lap.getLaptopId()==id);
+		}
 	}
 
 
